14405b101a8d9f8c60553b1bbea93bcccf9a8495
NSSEI <58462575+NSSEI@users.noreply.github.com>
2020-11-17 03:07:22 +0900

    Update destination_search.py

--- destination_search.py
+++ destination_search.py
@@ -1,5 +1,7 @@
 import requests, bs4
 import pandas as pd
+import SeoulBusAPI
+import numpy as np
 from urllib.parse import urlencode, quote_plus, unquote
 
 # 판다스 데이터 행 표시 100까지 / 열 표시 10까지
@@ -7,7 +9,8 @@ pd.set_option('display.max_row', 100)
 pd.set_option('display.max_column', 10)
 
 # API 서비스키 값
-My_API_Key = unquote('bhQZulxZSz%2FeMsDseMe2DSccTVB%2BQPnxTxDp4SrK7HYRP%2BS0YDiBn93FLz0d%2FMFbyMPUqAvaMqrtW4e9%2FnHYhA%3D%3D')
+my_api_key = unquote('bhQZulxZSz%2FeMsDseMe2DSccTVB%2BQPnxTxDp4SrK7HYRP%2BS0YDiBn93FLz0d%2FMFbyMPUqAvaMqrtW4e9%2FnHYhA%3D%3D')
+service_key = "SVpCy0bvZ5pGxpQdz6HmdUFgFl5L6vUbmK9tzQAPslFjjRHSBsKGTvYAkRC84aHoeUct2mtsiD8YfWyEzOQMIQ%3D%3D"
 
 
 class GetLocation:
@@ -17,7 +20,7 @@ class GetLocation:
 
         xmlUrl = 'http://ws.bus.go.kr/api/rest/pathinfo/getLocationInfo'
 
-        queryParams = '?' + urlencode({quote_plus('ServiceKey'): My_API_Key,
+        queryParams = '?' + urlencode({quote_plus('ServiceKey'): my_api_key,
                                        quote_plus('stSrch'): stt_name})
 
         response = requests.get(xmlUrl + queryParams).text.encode('utf-8')
@@ -37,7 +40,7 @@ class PathData:
 
         xmlUrl = 'http://ws.bus.go.kr/api/rest/pathinfo/getPathInfoByBusNSub'
 
-        queryParams = '?' + urlencode({quote_plus('ServiceKey'): My_API_Key,
+        queryParams = '?' + urlencode({quote_plus('ServiceKey'): my_api_key,
                                        quote_plus('startX'): start_x,
                                        quote_plus('startY'): start_y,
                                        quote_plus('endX'): end_x,
@@ -80,6 +83,8 @@ class GetStationByBusRoute:
         stationNm = []
         gpsX = []
         gpsY = []
+        sectSpd = []
+        fullSectDist = []
 
         bus_route_info = {}
 
@@ -88,7 +93,7 @@ class GetStationByBusRoute:
         My_API_Key = unquote(
             'bhQZulxZSz%2FeMsDseMe2DSccTVB%2BQPnxTxDp4SrK7HYRP%2BS0YDiBn93FLz0d%2FMFbyMPUqAvaMqrtW4e9%2FnHYhA%3D%3D')
 
-        queryParams = '?' + urlencode({quote_plus('ServiceKey'): My_API_Key,
+        queryParams = '?' + urlencode({quote_plus('ServiceKey'): my_api_key,
                                        quote_plus('busRouteId'): self.bus_route})
 
         response = requests.get(xmlUrl + queryParams).text.encode('utf-8')
@@ -105,6 +110,8 @@ class GetStationByBusRoute:
             stationNm.append(itemList_tag[i].find('stationNm').text)
             gpsX.append(itemList_tag[i].find('gpsX').text)
             gpsY.append(itemList_tag[i].find('gpsY').text)
+            sectSpd.append(itemList_tag[i].find('sectSpd').text)
+            fullSectDist.append(itemList_tag[i].find('fullSectDist').text)
 
 
             bus_route_info['진행방향'] = direction
@@ -115,11 +122,97 @@ class GetStationByBusRoute:
             bus_route_info['정류장이름'] = stationNm
             bus_route_info['gpsX'] = gpsX
             bus_route_info['gpsY'] = gpsY
+            bus_route_info['구간속도'] = sectSpd
+            bus_route_info['구간거리'] = fullSectDist
 
             df = pd.DataFrame(bus_route_info)
 
-            df = df[['진행방향', '순번', '구간ID', '정류소ID', '정류소번호', '정류장이름', 'gpsX', 'gpsY']]
+            df = df[['진행방향', '순번', '구간ID', '정류소ID', '정류소번호', '정류장이름', 'gpsX', 'gpsY', '구간속도', '구간거리']]
 
         return df
 
 
+class DestinationStation:
+
+
+    def __init__(self, gps_x, gps_y, time):
+
+        self.gps_x = gps_x
+        self.gps_y = gps_y
+        self.time = time
+
+    def result_station(self):
+
+        bus_route = []
+        station_gps = []
+
+        bst = SeoulBusAPI.BusStation(service_key)
+        station_list = bst.findStation(self.gps_x, self.gps_y)
+        print(station_list)  # 근처 정류장 모두 찾기
+
+        for station in station_list:
+            get_route = SeoulBusAPI.BusRoute(service_key).getRoute(station)
+            for i in get_route:
+                if i not in bus_route:
+                    if i[0] == '1':
+                        # 오류나는 버스 노선 제거 (오류나는 버스 노선이 2로 시작)
+                        bus_route.append(i)
+
+        print(bus_route)  # 정류장이 지나는 버스 노선 찾기
+
+        for route in bus_route:
+
+            # api에서 제공하는 구간거리, 구간속도를 기준으로 도착 정류장 계산
+
+            df = GetStationByBusRoute(route).search()
+            station_num = list(np.array(df['정류소번호'].tolist()))
+            bus_direction = list(np.array(df['진행방향'].tolist()))
+            gps_x = list(np.array(df['gpsX'].tolist()))
+            gps_y = list(np.array(df['gpsY'].tolist()))
+            sect_spd = list(np.array(df['구간속도'].tolist()))
+            sect_dist = list(np.array(df['구간거리'].tolist()))
+            station_name = list(np.array(df['정류장이름'].tolist()))
+
+            for station in station_list:
+
+                req_time = 0
+                cnt = 1
+
+                if station in station_num:
+                    start_index = station_num.index(station)
+
+                    # 도착 정류장 계산
+                    while True:
+                        # 속도를 km/h에서 m/m으로 수정
+                        meter_per_min = int(sect_spd[start_index + cnt]) * 1000 / 60
+                        req_time += int(sect_dist[start_index + cnt]) / meter_per_min
+                        if req_time > self.time:
+                            req_time = int(req_time)
+                            break
+                        else:
+                            cnt += 1
+
+                    dest_index = start_index + cnt - 1
+                    if len(station_num) - 1 < dest_index:
+                        break
+
+                    if bus_direction[start_index] == bus_direction[dest_index]:
+                        # 버스가 회차한 경우를 제외하기 위해 방향이 같은 정류장 선택
+                        stt_name = station_name[dest_index]
+                        one_result = [gps_x[dest_index],gps_y[dest_index], req_time, stt_name]
+
+                        if one_result not in station_gps:
+                            # 중복되는 결과 제외
+                            station_gps.append(one_result)
+                        '''
+                        print(gps_x[start_index], gps_y[start_index], gps_x[dest_index], gps_y[dest_index])
+                        print('소요시간 : ', req_time)
+                        print('도착 정류장 : ' + stt_name)
+                        '''
+
+        return station_gps
+
+    def show_result(self):
+        station_list = self.result_station()
+        for i in station_list:
+            print(i)
